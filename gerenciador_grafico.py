d4874984eabb7efb263c7d52f1101ac56f9a3fc6
Bruno <bruno.valalves@gmail.com>
2022-11-06 17:55:53 -0300

    oi

---
+++ gerenciador_grafico.py
@@ -0,0 +1,10 @@
+class GerenciadorGrafico:
+    def __init__(self, dados):
+        self.dados = dados
+    
+    def desenhar_histograma(self):
+        print ('desenhar histograma')
+        pass
+    
+    def desenhar_dispersao(self):
+        pass
