b43366ebc28e011ddabdcb561ec601d211bc3010
Bruno <bruno.valalves@gmail.com>
2023-01-09 22:09:07 -0300

    LAST UPDATES

--- gerenciador_grafico.py
+++ gerenciador_grafico.py
@@ -12,9 +12,9 @@ class GerenciadorGrafico:
         times_1,times_2 = 0,0
         for dado in self.dados:
             if dado[4] == 1:
-                times_1 = times_1 + 1
+                times_1 += 1
             elif dado[4] == 2:
-                times_2 = times_2 + 1            
+                times_2 += 1            
 
         np.random.seed(1)
         x = [1,2]
@@ -24,23 +24,36 @@ class GerenciadorGrafico:
         
         fig, ax = plt.subplots()
         # ax.hist(self)#.dados)
-        bar = ax.bar(x, y, width=1, edgecolor="white")
+        bar = ax.bar(x, y, width=1, edgecolor="white", color = (.76, .12, .98))
         ax.bar_label(bar)
+        plt.xlabel("Times", size = 12, color = (.76, .12, .98))
+        plt.xticks(range(1, 3, 1))
+        plt.ylabel("Vitórias", size = 12, color = (.37, .98, .20))
+
+        plt.title("Vitórias por time", 
+          fontdict={'family': 'serif', 
+                    'color' : 'orange',
+                    'weight': 'bold',
+                    'size': 16})
         plt.show()
             
-    def desenhar_dispersao(self,dados):
 
-        # make data: correlated + noise
-        np.random.seed(1)
-        x = np.arange(0,125,5)
-        y = np.random.random(len(x))
 
-        plt.scatter(x,y, color = 'c', marker ='*')
-        plt.xlabel('eixo x', fontsize = 16)
-        plt.ylabel('eixo y',fontsize = 16)
-        plt.title ('dispersão',fontsize = 20)
-        plt.xlim([0,125])
-        plt.ylim([0,1])
 
-        plt.show()
-        pass
+
+    # def desenhar_dispersao(self,dados):
+
+    #     # make data: correlated + noise
+    #     np.random.seed(1)
+    #     x = np.arange(0,125,5)
+    #     y = np.random.random(len(x))
+
+    #     plt.scatter(x,y, color = 'c', marker ='*')
+    #     plt.xlabel('eixo x', fontsize = 16)
+    #     plt.ylabel('eixo y',fontsize = 16)
+    #     plt.title ('dispersão',fontsize = 20)
+    #     plt.xlim([0,125])
+    #     plt.ylim([0,1])
+
+    #     plt.show()
+    #     pass
--- main.py
+++ main.py
@@ -16,26 +16,6 @@ cursor.execute('select * from public.matches')
 
 dados = cursor.fetchall()
 
-# dados=[
-#     (2357689, 'aravt-vs-d13-5e-arena-asia-cup-2022-closed-qualifier', 1.7, 1.95, 1),
-#     (2357543, 'furia-academy-vs-astralis-talent-weplay-academy-league-season-5', 0, 0, 2),
-#     (2357690, 'nkt-vs-lynn-vision-5e-arena-asia-cup-2022-closed-qualifier', 2.42, 1.55, 2),
-#     (2357544, 'spirit-academy-vs-og-academy-weplay-academy-league-season-5', 1.52, 2.49, 1),
-#     (2357691, 'catevil-vs-ihc-5e-arena-asia-cup-2022-closed-qualifier', 5.5, 1.1, 1),
-#     (2357753, 'wolsung-vs-cryptova-cct-central-europe-series-1-closed-qualifier', 0, 0, None),
-#     (2357545, 'navi-junior-vs-furia-ac-astralis-t-winner-weplay-academy-league-season-5', 0, 0, None),
-#     (2357776, 'daotsu-vs-stars-horizon-cct-south-america-series-1', 1.52, 5.25, None),
-#     (2357822, 'ec-kyiv-vs-yodagus-elisa-invitational-fall-2022-contenders', 1.13, 6.0, None),
-#     (2357877, 'order-vs-encore-iem-road-to-rio-2022-oceania-open-qualifier-1', 1.3, 4.2, None),
-#     (2357878, 'rooster-vs-morbin5-iem-road-to-rio-2022-oceania-open-qualifier-1', 1.85, 2.8, None),
-#     (2357883, 'rare-atom-vs-wings-up-iem-road-to-rio-2022-china-open-qualifier-1', 1.46, 2.65, None),
-#     (2357884, 'lynn-vision-vs-tyloo-iem-road-to-rio-2022-china-open-qualifier-1', 4.81, 1.17, None),
-#     (2357889, 'ihc-vs-d13-iem-road-to-rio-2022-asia-open-qualifier-1', 1.14, 5.3, None),
-#     (2357890, 'nkt-vs-aravt-iem-road-to-rio-2022-asia-open-qualifier-1', 1.48, 2.77, None),
-#     (2357823, 'b8-vs-777-elisa-invitational-fall-2022-contenders', 1.5, 2.7, None)
-# ]
-
-
 gerenciador=GerenciadorGrafico(dados)
 gerenciador.desenhar_histograma()
 
